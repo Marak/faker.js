53d5f812e6e18fd79df442c8f6bdbfacda3bbd5b
Rodrigo Machado <rcmachado@gmail.com>
2012-01-24 12:04:33 -0200

    Fix incorrect merge

--- Faker.js
+++ Faker.js
@@ -67,46 +67,46 @@ Faker.Name.lastName = function () {
 
 Faker.Person = {};
 Faker.Person.brCPF = function () {
-	var baseNumber = Helpers.randomize('###.###.###'),
-		number = Helpers.replaceSymbolWithNumber(baseNumber),
-		firstDigit, secondDigit, parts;
-	
-	parts = number.split('');
+  var baseNumber = Helpers.randomize('###.###.###'),
+    number = Helpers.replaceSymbolWithNumber(baseNumber),
+    firstDigit, secondDigit, parts;
+  
+  parts = number.split('');
 
-	// Computing first verification digit
-	firstDigit = 10 * parts[0] +
-		9 * parts[1] +
-		8 * parts[2] +
-		7 * parts[4] +
-		6 * parts[5] +
-		5 * parts[6] +
-		4 * parts[8] +
-		3 * parts[9] +
-		2 * parts[10];
+  // Computing first verification digit
+  firstDigit = 10 * parts[0] +
+    9 * parts[1] +
+    8 * parts[2] +
+    7 * parts[4] +
+    6 * parts[5] +
+    5 * parts[6] +
+    4 * parts[8] +
+    3 * parts[9] +
+    2 * parts[10];
 
-	firstDigit = 11 - (firstDigit % 11);
-	if (firstDigit >= 10) {
-		firstDigit = 0;
-	}
+  firstDigit = 11 - (firstDigit % 11);
+  if (firstDigit >= 10) {
+    firstDigit = 0;
+  }
 
-	// Computing second verification digit
-	secondDigit = 11 * parts[0] +
-		10 * parts[1] +
-		9 * parts[2] +
-		8 * parts[4] +
-		7 * parts[5] +
-		6 * parts[6] +
-		5 * parts[8] +
-		4 * parts[9] +
-		3 * parts[10] +
-		2 * firstDigit;
+  // Computing second verification digit
+  secondDigit = 11 * parts[0] +
+    10 * parts[1] +
+    9 * parts[2] +
+    8 * parts[4] +
+    7 * parts[5] +
+    6 * parts[6] +
+    5 * parts[8] +
+    4 * parts[9] +
+    3 * parts[10] +
+    2 * firstDigit;
 
-	secondDigit = 11 - (secondDigit % 11);
-	if (secondDigit >= 10) {
-		secondDigit = 0;
-	}
+  secondDigit = 11 - (secondDigit % 11);
+  if (secondDigit >= 10) {
+    secondDigit = 0;
+  }
 
-	return number + '-' + firstDigit + secondDigit;
+  return number + '-' + firstDigit + secondDigit;
 };
 
 Faker.Address = {};
@@ -413,94 +413,35 @@ Faker.definitions.name_prefix = function (){return ["Mr.", "Mrs.", "Ms.", "Miss"
 Faker.definitions.name_suffix = function (){return ["Jr.", "Sr.", "I", "II", "III", "IV", "V", "MD", "DDS", "PhD", "DVM"];};
 
 Faker.definitions.br_state = function () {
-<<<<<<< HEAD
-	return [
-		'Acre',
-		'Alagoas',
-<<<<<<< HEAD
-		'Amapá',
-		'Amazonas',
-		'Bahia',
-		'Ceará',
-		'Distrito Federal',
-		'Espírito Santo',
-		'Goiás',
-		'Maranhão',
-		'Mato Grosso',
-		'Mato Grosso do Sul',
-		'Minas Gerais',
-		'Paraná',
-		'Paraíba',
-		'Pará',
-		'Pernambuco',
-		'Piauí',
-		'Rio de Janeiro',
-		'Rio Grande do Norte',
-		'Rio Grande do Sul',
-		'Rondônia',
-		'Roraima',
-		'Santa Catarina',
-		'Sergipe',
-		'São Paulo',
-=======
-		'Amap�',
-		'Amazonas',
-		'Bahia',
-		'Cear�',
-		'Distrito Federal',
-		'Esp�rito Santo',
-		'Goi�s',
-		'Maranh�o',
-		'Mato Grosso',
-		'Mato Grosso do Sul',
-		'Minas Gerais',
-		'Paran�',
-		'Para�ba',
-		'Par�',
-		'Pernambuco',
-		'Piau�',
-		'Rio de Janeiro',
-		'Rio Grande do Norte',
-		'Rio Grande do Sul',
-		'Rond�nia',
-		'Roraima',
-		'Santa Catarina',
-		'Sergipe',
-		'S�o Paulo',
->>>>>>> cea0735c9a118179623eba9b10df49dde7c6963b
-		'Tocantins'
-	];
-=======
   return [
     'Acre',
     'Alagoas',
-    'Amap�',
+    'Amapá',
     'Amazonas',
     'Bahia',
-    'Cear�',
+    'Ceará',
     'Distrito Federal',
-    'Esp�rito Santo',
-    'Goi�s',
-    'Maranh�o',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
     'Mato Grosso',
     'Mato Grosso do Sul',
     'Minas Gerais',
-    'Paran�',
-    'Para�ba',
-    'Par�',
+    'Paraná',
+    'Paraíba',
+    'Pará',
     'Pernambuco',
-    'Piau�',
+    'Piauí',
     'Rio de Janeiro',
     'Rio Grande do Norte',
     'Rio Grande do Sul',
-    'Rond�nia',
+    'Rondônia',
     'Roraima',
     'Santa Catarina',
     'Sergipe',
-    'S�o Paulo',
+    'São Paulo',
     'Tocantins'
   ];
->>>>>>> upstream/master
 };
 
 Faker.definitions.br_state_abbr = function () {
