2ea888c45a7487b5c5405b51d8ca24b28a5ba01a
vamshi krishna <saikrishana9@gmail.com>
2020-03-14 19:12:38 +0530

    index Modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
-console.log(faker.movie.director());
-console.log(faker.movie.actor());
-console.log(faker.movie.genre());
+console.log("Director: "+faker.movie.director());
+console.log("Actor: "+faker.movie.actor());
+console.log("Actress: "+faker.movie.actress());
+console.log("Genre: "+faker.movie.genre());
