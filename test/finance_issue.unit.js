39cfbab969e5f0d7bc7f5e3ea3e3b6ae4c898c18
Christophe Gesché <moosh@php.net>
2020-08-05 10:39:29 +0200

    Update finance_issue.unit.js
    use an home made function to check if parts is numeric.

--- test/finance_issue.unit.js
+++ test/finance_issue.unit.js
@@ -1,12 +1,11 @@
 if (typeof module !== 'undefined') {
     var assert = require('assert');
-    var chai = require('./support/chai.js').assert;
     var sinon = require('sinon');
     var faker = require('../index');
 }
 
-faker.setLocale('pk');
-faker.seed(28);
+//faker.setLocale('pk');
+faker.seed(28); // I need to found how to randomly have always a pk iban
 describe('finance_issue.js', function () {
     describe("issue_945 IBAN Pakistan", function () {
 
@@ -22,16 +21,26 @@ describe('finance_issue.js', function () {
         it("IBAN for Pakistan not correct", function () {
             var iban = faker.finance.iban();
             var bban = iban.substring(4) + iban.substring(0, 4);
-            assert.equal(iban.substring(0,2),'PK');
 
-            // chai.assert.isNumber(iban.substring(4,8));
-            // chai.assert.isNumber(iban.substring(8,12));
-            // chai.assert.isNumber(iban.substring(12,16));
-            // chai.assert.isNumber(iban.substring(16,20));
-            // chai.assert.isNumber(iban.substring(20,24));
+            assert.equal(iban.substring(0,2),'PK');
+            assert.ok(false === IsNumeric(iban.substring(0,2)));
+            //assert.ok(false === IsNumeric(iban.substring(4,8)));
+
+
+            assert.ok(IsNumeric(iban.substring(2,4)));
+            assert.ok(IsNumeric(iban.substring(8,12)));
+            assert.ok(IsNumeric(iban.substring(12,16)));
+            assert.ok(IsNumeric(iban.substring(16,20)));
+            assert.ok(IsNumeric(iban.substring(20,24)));
+            assert.ok(IsNumeric(iban.substring(8,24)));
 
             assert.equal(ibanLib.mod97(ibanLib.toDigitString(bban)), 1, "the result should be equal to 1");
         });
     });
 
 });
+
+function IsNumeric(input)
+{
+    return (input - 0) == input && (''+input).trim().length > 0;
+}
