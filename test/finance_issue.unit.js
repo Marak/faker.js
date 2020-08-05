2730e32f9a97f4d630c5deee50a962d7599565d0
Christophe Gesché <moosh@php.net>
2020-08-05 13:23:32 +0200

    Update finance_issue.unit.js
    -  no use of sinon
    - randmise and test with first PK
    - A999 is not valable

--- test/finance_issue.unit.js
+++ test/finance_issue.unit.js
@@ -1,11 +1,8 @@
 if (typeof module !== 'undefined') {
     var assert = require('assert');
-    var sinon = require('sinon');
     var faker = require('../index');
 }
 
-//faker.setLocale('pk');
-faker.seed(28); // I need to found how to randomly have always a pk iban
 describe('finance_issue.js', function () {
     describe("issue_945 IBAN Pakistan", function () {
 
@@ -18,20 +15,28 @@ describe('finance_issue.js', function () {
 
         var ibanLib = require('../lib/iban');
 
-        it("IBAN for Pakistan not correct", function () {
+        it("IBAN for Pakistan is correct", function () {
             var iban = faker.finance.iban();
+            var maxTry = 100000;
+            while (maxTry && iban.substring(0,2) != 'PK' ) {
+              faker.seed();
+              var iban = faker.finance.iban();
+              //console.log(iban);
+              maxTry--;
+            }
+
+            if (maxTry==0) {
+              console.log('aucun PK dans les 10000 seed, vraiment pas de bol');
+            }
+
             var bban = iban.substring(4) + iban.substring(0, 4);
 
-            assert.equal(iban.substring(0,2),'PK');
-            assert.ok(false === IsNumeric(iban.substring(0,2)));
-            //assert.ok(false === IsNumeric(iban.substring(4,8)));
-
+            assert.ok(false === IsNumeric(iban.substring(4,5)), iban.substring(4,8)+' contains only characters in PK Iban');
+            assert.ok(false === IsNumeric(iban.substring(5,6)), iban.substring(4,8)+' contains only characters in PK Iban');
+            assert.ok(false === IsNumeric(iban.substring(6,7)), iban.substring(4,8)+' contains only characters in PK Iban');
+            assert.ok(false === IsNumeric(iban.substring(7,8)), iban.substring(4,8)+' contains only characters in PK Iban');
 
             assert.ok(IsNumeric(iban.substring(2,4)));
-            assert.ok(IsNumeric(iban.substring(8,12)));
-            assert.ok(IsNumeric(iban.substring(12,16)));
-            assert.ok(IsNumeric(iban.substring(16,20)));
-            assert.ok(IsNumeric(iban.substring(20,24)));
             assert.ok(IsNumeric(iban.substring(8,24)));
 
             assert.equal(ibanLib.mod97(ibanLib.toDigitString(bban)), 1, "the result should be equal to 1");
